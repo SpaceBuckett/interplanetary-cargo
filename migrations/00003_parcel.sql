ced26bc3d70d551261bb61285b92cb2d13fa3241
spacebuckett <shehxadv2.0@gmail.com>
2025-12-19 09:43:19 +0500

    added parcel migration

---
+++ migrations/00003_parcel.sql
@@ -0,0 +1,20 @@
+-- +goose up
+-- +goose StatementBegin 
+CREATE TABLE IF NOT EXISTS parcel(
+    id BIGSERIAL PRIMARY KEY, 
+    from_station VARCHAR(255) NOT NULL, 
+    to_station VARCHAR(255) NOT NULL,  
+    pickup_address TEXT NOT NULL, 
+    delivery_address TEXT NOT NULL, 
+    contact TEXT,   
+    special_notes TEXT,  
+    weight NUMBER, 
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,  
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,  
+) 
+-- +goose StatementEnd
+
+-- +goose down 
+-- +goose StatementBegin
+DROP TABLE parcel;  
+-- +goose statementEnd
